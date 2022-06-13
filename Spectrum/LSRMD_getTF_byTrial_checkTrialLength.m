0c64a4ab231e340e4d6c9ca61eb9bc7c1e5d7fbd
Thomas Andrillon <31892797+andrillon@users.noreply.github.com>
2022-06-13 09:36:19 +0200

    Create LSRMD_getTF_byTrial_checkTrialLength.m

---
+++ Spectrum/LSRMD_getTF_byTrial_checkTrialLength.m
@@ -0,0 +1,64 @@
+%%
+clear all;
+close all;
+
+%%
+run ../LS_RMD_localdef.m
+addpath((path_fieldtrip));
+ft_defaults;
+rmpath(genpath([path_fieldtrip filesep 'external' filesep 'signal' ]))
+% 
+addpath(genpath(path_LSCPtools));
+addpath(genpath(path_IRASA))
+files=dir([preproc_path filesep 'ICAcleaned_etrial_ft_*.mat']);
+
+%DP - in case I want to split by cohort
+% CohYoungM   =   {'036M_JK','037M_JD','059M_HP','061M_LG','068M_CB','091M_SW','093M_BR','103M_JK','106M_NH','108M_CY',...
+%                  '114M_CS','118M_CS','133M_DC','134M_JM','136M_JC','147M_EB','186M_AF','189M_WM','191M_DM','203M_VF',...
+%                  '205M_LE','220M_NB','221M_SJ','226M_SM','234M_SW','235M_JM','240M_FM','243M_JB','265M_EZ','279M_FT',...
+%                  '289M_AS','291M_KS','301M_MO','302M_BS','303M_SA','314M_LK','323M_CZ','325M_KR','328M_EW','331M_CL',...
+%                  '338M_SC','339M_YV','352M_MK','353M_AT','374M_PP','377M_BL','378M_MG','384M_PD','392M_PH','398M_LO',...
+%                  '400M_ED','404M_RO','414M_LA','418M_AM','422M_MK','427M_SS','439M_TM','453M_LB','458M_AH','484M_AI'};
+% CohYoungT   =   {'AA_15_04_14','AC_13_05_14','AR_08_04_14','AS_23_04_14','EL_24_05_14','GW_09_05_14','JC_23_05_14',...
+%                  'LK_07_04_14','MH_14_04_14','ND_16_05_14','NT_16_04_14','OF_28_04_14','OM_07_05_14','OS_09_05_14',...
+%                  'PR_20_04_14','RM_06_05_14','RO_25_04_14','SB_08_05_14','SF_20_05_14','SH_25_05_14','TL_23_05_14'};
+% CohYoungHN  =   {'HN867','HN868','HN870','HN871','HN872','HN873','HN874','HN875','HN876','HN877','HN878','HN879',...
+%                  'HN880','HN881','HN882','HN883','HN884','HN885','HN886','HN888','HN889','HN890','HN891','HN892',...
+%                  'HN893','HN894','HN895','HN896','HN897','HN898','HN899'};
+% CohOldA     =   {'A101','A102','A103','A104','A105','A106','A107','A108','A110','A111','A112','A113','A114','A115',...
+%                  'A116','A117','A118','A119','A120'};
+% CohOldHN    =   {'HN968','HN969','HN970','HN971','HN972','HN973','HN974','HN976','HN977','HN978','HN980','HN981',...
+%                  'HN982','HN983','HN985','HN986','HN987','HN988','HN989','HN990','HN992','HN993','HN994','HN995',...
+%                  'HN996','HN998','HN999'};
+
+%%
+res_mat=[];
+redo=0; complete=0;
+
+% m = 1; t = 1; h = 1; a = 1; hn = 1;
+%
+% YoungM_pow=[]; YoungM_SNR=[]; YoungM_SNRtag=[]; YoungT_pow=[]; YoungT_SNR=[]; YoungT_SNRtag=[]; YoungHN_pow=[]; YoungHN_SNR=[]; YoungHN_SNRtag=[];
+% OldA_pow=[]; OldA_SNR=[]; OldA_SNRtag=[]; OldHN_pow=[]; OldHN_SNR=[]; OldHN_SNRtag=[];
+absThr=250;
+nFc=0;
+for nF=1:length(files)
+    file_name = files(nF).name;
+    folder_name = files(nF).folder;
+    SubID=file_name(1:end-4);
+    seps=findstr(SubID,'ft_');
+    SubID=SubID(seps(1)+3:end);
+    tic;
+    fprintf('... working on %s (%g/%g)\n',SubID,nF,length(files))
+    
+    if redo==1 || exist([preproc_path filesep SubID '_TF_perTrial_ICAcleaned.mat'])==0
+        subj_pow=[];
+
+        
+        load([folder_name filesep file_name]);
+        
+        
+        fprintf('... found % trials of length %g s\n',length(data.trial),size(data.trial{1},2)/data.fsample)
+    end
+end
+
+
